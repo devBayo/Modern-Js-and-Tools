7ad5833e1f3feec14a80456f4a508589d261c24a
hashvector <ibrahimabdulqoyyum@gmail.com>
2022-09-21 09:28:49 +0100

    the module pattern

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+/*
 // Importing module
 console.log('importing modules');
 // import { addToCart } from './shoppingCart.js';
@@ -18,3 +19,34 @@ add('pizza', 9);
 add('bread', 2);
 add('orange', 5);
 console.log(cart); // Imports are live connections
+
+*/
+
+// The previous module pattern
+const ShoppingCart2 = (function () {
+  const cart = [];
+  const totalPrice = 20;
+  const totalQuantity = 200;
+  const shippingCost = 500;
+
+  const addToCart = function (product, quantity) {
+    cart.push({ product, quantity });
+    console.log(quantity, product);
+  };
+
+  const orderStock = function (product, quantity) {
+    console.log(quantity, product);
+  };
+
+  return {
+    cart,
+    totalPrice,
+    totalQuantity,
+    addToCart,
+  };
+})();
+
+console.log(ShoppingCart2);
+ShoppingCart2.addToCart('bread', 8);
+console.log(ShoppingCart2.cart);
+// console.log(ShoppingCart2.shopStuffs);
