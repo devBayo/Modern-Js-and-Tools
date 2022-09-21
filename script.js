d0d2de08ab23cbd3a8c428740e7f65a8bbea490a
hashvector <ibrahimabdulqoyyum@gmail.com>
2022-09-21 08:57:51 +0100

    working with modules

--- script.js
+++ script.js
@@ -1,11 +1,20 @@
 // Importing module
+console.log('importing modules');
 // import { addToCart } from './shoppingCart.js';
 // import { totalPrice as price, quantity } from './shoppingCart.js';
 // addToCart('bread', 2);
 // console.log(price, quantity);
 
-console.log('Importing Module');
-import * as ShoppingCart from './shoppingCart.js';
+// console.log('Importing Module');
+// import * as ShoppingCart from './shoppingCart.js';
 
-ShoppingCart.addToCart('Oranges', 4);
-console.log(ShoppingCart.totalPrice);
+// ShoppingCart.addToCart('Oranges', 4);
+// console.log(ShoppingCart.totalPrice);
+
+// importing default
+import add from './shoppingCart.js';
+import { cart } from './shoppingCart.js';
+add('pizza', 9);
+add('bread', 2);
+add('orange', 5);
+console.log(cart);
--- shoppingCart.js
+++ shoppingCart.js
@@ -1,7 +1,7 @@
 // Exporting Module
 console.log('Exporting Module');
 
-const cart = [];
+export const cart = [];
 
 export const addToCart = function (product, quantity) {
   cart.push({ product, quantity });
@@ -12,3 +12,9 @@ const totalPrice = 200;
 const totalQuantity = 20;
 
 export { totalPrice, totalQuantity as quantity };
+
+// default values
+export default function (product, quantity) {
+  cart.push({ product, quantity });
+  console.log(`${quantity} ${product} added to cart`);
+}
